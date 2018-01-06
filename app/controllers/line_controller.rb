f707f4e2ad3b69170e9d4b8aed1a203a2765c5f2
Ryuki Wada <xim.ryuki@gmail.com>
2018-01-06 17:59:19 +0900

    make a line_controller

---
+++ app/controllers/line_controller.rb
@@ -0,0 +1,39 @@
+class LineController < ApplicationController
+  require 'line/bot'
+  protect_from_forgery :except => [:callback]
+
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+
+    events.each do |event|
+      case event
+        when Line::Bot::Event::Message
+          case event.type
+            when Line::Bot::Event::MessageType::Text
+              message = {
+                  type: 'text',
+                  text: event.message['text']
+              }
+              client.reply_message(event['replyToken'], message)
+          end
+      end
+    end
+    "OK"
+  end
+
+  private
+  def client
+    @client ||= Line::Bot::Client.new {|config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+end
