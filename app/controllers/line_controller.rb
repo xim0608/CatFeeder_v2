c9c33bdf1c12181e22ef9c4f9e884d0bc6d8f97a
Ryuki Wada <xim.ryuki@gmail.com>
2018-01-06 18:17:39 +0900

    fix controller

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -6,7 +6,7 @@ class LineController < ApplicationController
   def callback
     body = request.body.read
 
-    signature = request.env['HTTP_LINE_SIGNATURE']
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
     end
